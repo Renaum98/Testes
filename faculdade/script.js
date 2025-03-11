fcf344b8c4a33a4e0031f654f73823c94a231d06
Renaum98 <167568624+Renaum98@users.noreply.github.com>
2025-03-11 17:15:14 -0300

    qwe

---
+++ faculdade/script.js
@@ -0,0 +1,7 @@
+const boxMenu = document.querySelector(".menu")
+const boxMenuBt = document.querySelector("#menu-icon")
+
+boxMenuBt.addEventListener("click", (evento) => {
+    evento.preventDefault()
+    
+})
