9b97515c2f9d3c04836b0bde708e85038a7c956a
Renaum98 <167568624+Renaum98@users.noreply.github.com>
2025-07-31 08:47:33 -0300

    upgrade

--- classificacao/main.js
+++ classificacao/main.js
@@ -1,11 +1,30 @@
-const listaContainer = document.querySelectorAll(".categorias-list")
+/*const listaContainer = document.querySelectorAll(".categorias-list")
+const caixaClasse = document.querySelectorAll(".caixa-classe")
 
 listaContainer.forEach(item => {
-    item.onclick = () =>{
-        if (item.style.display === "none") {
+    caixaClasse.onclick = () =>{
+        if (item.style.display === "none" || item.style.display == "") {
             item.style.display = "block"
-        } else {
+        } 
+        else {
             item.style.display = "none"
         }
     }
-})
+})*/
+
+const caixas = document.querySelectorAll(".caixa-classe");
+const listaContainer = document.querySelectorAll(".categorias-list");
+
+caixas.forEach((caixa, index) => {
+  const lista = listaContainer[index];
+  caixa.addEventListener("click", () => {
+    const current = getComputedStyle(lista).display;
+
+    if (current === "none") {
+      lista.style.display = "flex";
+    } else {
+      lista.style.display = "none";
+    }
+  });
+});
+
